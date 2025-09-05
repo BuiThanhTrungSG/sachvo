a41136bb8754c979b046c4edcfe034907e98c295
= <=>
2025-09-05 21:15:53 +0700

    ok

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// tăng limit lên 500mb
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
 
 // middleware để gắn io vào req (cho phép controller dùng io.emit)
 app.use((req, res, next) => {
